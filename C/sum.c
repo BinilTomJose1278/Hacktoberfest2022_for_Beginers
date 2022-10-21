3ab288e62f7c065fcc21c280f09650ee4746560f
BinilTomJose1278 <biniltomjose12780@gmail.com>
2022-10-21 18:45:23 +0530

    Sum of numbers for array values were done

---
+++ C/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+   int sum=0,i,n,A[100];
+   printf("Enter the number");
+   scanf("%d",&n);
+   printf("Enter the array values");
+   for(i=0;i<n;i+){
+    sum+=A[i];
+   }
+   printf("Sum is %d",sum);
+
+}
